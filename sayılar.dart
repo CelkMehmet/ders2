daacdcf6a33e97390f6194b1adbe004fce570ef0
MehmetCelik <67478048+CelkMehmet@users.noreply.github.com>
2024-03-27 11:29:44 +0300

    Add files via upload

---
+++ sayılar.dart
@@ -0,0 +1,22 @@
+void main(List<String> args) {
+  int yas = 35;
+  yas=48;
+  print(yas);
+
+  int isi =42;
+  isi=48;
+  print(isi);
+
+  double guc ;
+  guc=89.2;
+  print(guc);
+
+  num en;
+  en= 92.2;
+  print(en);
+
+
+  var yil = 1883;
+  print(yil);
+
+}
